59ffd813aca0a007f42a29a811f89a8dfe8c4036
liangjiaxin <liangjiaxin@e-byte.com>
2025-08-07 15:28:39 +0800

    Initial Commit

---
+++ Jx今夏/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Jx今夏
+//
+//  Created by 嘉煦 on 2025/8/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Jx今夏/Jx__App.swift
@@ -0,0 +1,17 @@
+//
+//  Jx__App.swift
+//  Jx今夏
+//
+//  Created by 嘉煦 on 2025/8/7.
+//
+
+import SwiftUI
+
+@main
+struct Jx__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
